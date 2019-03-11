20f80a6c4e6ae5f723ffa0daa83cecfe20fc5c74
Mathias Maaß <mathias.maass@uni-leipzig.de>
2019-03-11 14:53:57 +0100

    --

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,27 @@ module.exports = {
   },
   //devtool: 'inline-source-map',
   plugins: [ new BundleAnalyzerPlugin() ],
+  resolve: {
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    }
+  },
+  externals: {
+    // Don't bundle react or react-dom
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React"
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",          
+      amd: "ReactDOM",
+      root: "ReactDOM"
+    }
+  },
   module: {
     rules: [
       {
